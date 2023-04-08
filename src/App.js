c275d565921d8f3a883440acc16172a1b9c0a9b5
Domka28 <119631616+Domka28@users.noreply.github.com>
2023-04-08 17:35:00 +0200

    New Question and Start components, changes in CSS and add APi

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Start from "./components/Start"
+import Question from './components/Questions';
 
 
 function App() {
+  const [questions, setQuestions] = useState()
+
+
+  React.useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setQuestions(data.results[0])
+      }
+      )
+  }, [])
+
+
+
+  const newGame = () => {
+
+  }
+
   return (
     <div className="main-container">
       <Start />
-      <button className="start-btn">Start quiz</button>
+      <Question questions={questions} />
+      <button
+        className="start-btn"
+        onClick={newGame}
+      >Start quiz</button>
     </div>
 
   );
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
 
-export default function Question() {
+export default function Question(props) {
 
 
     return (
         <div className="question-container">
-            <h1>How would one say goodbye in Spanish?</h1>
+            <h1 className="question">How would one say goodbye in Spanish?</h1>
             <div className="answers">
-                <p>Adiós</p>
-                <p>Hola</p>
-                <p>Au Revoir</p>
-                <p>Salir</p>
+                <p className="answer">Adiós</p>
+                <p className="answer">Hola</p>
+                <p className="answer">Au Revoir</p>
+                <p className="answer">Salir</p>
             </div>
         </div>
     )
