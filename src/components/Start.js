c65f94cc521d7eb04d928e455f5b193b0db500d2
Domka28 <119631616+Domka28@users.noreply.github.com>
2023-04-18 16:09:21 +0200

    Adjust layout for mobile and desktop version

--- src/components/Start.js
+++ src/components/Start.js
@@ -5,7 +5,8 @@ export default function Start() {
 
     return (
         <div className="start-container">
-            <h1 className="quiz-title">Quizzical</h1>
+            <h1 className="quiz-title">Quiziti</h1>
+            <p className="start-sentence">Test your knowledge!</p>
         </div>
     )
 }
